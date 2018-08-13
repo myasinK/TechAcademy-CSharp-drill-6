c599b8f99b3ecc85c874e21ed34cc886fc63bcfe
myasinK <yasinul@gmail.com>
2018-08-13 16:10:18 -0700

    Finish drill on booleans

---
+++ ShippingQuote/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("Please enter package weight: ");
+            float weight = float.Parse( Console.ReadLine() );
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter package width: ");
+                float width = float.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter package height: ");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter package length: ");
+                float length = float.Parse(Console.ReadLine());
+                float quote = (width + length + height) * weight / 100.0f;
+
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    Console.WriteLine("Your estimated total for shipping is: $" + Convert.ToString( quote ) + "\nThank you.");
+                }
+            }
+        }
+    }
+}
